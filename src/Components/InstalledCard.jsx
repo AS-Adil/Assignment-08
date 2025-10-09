639f490606b614da293fdf9bae27ebac9b7c5a45
AS-Adil <abdussamadadil1@gmail.com>
2025-10-10 02:04:49 +0600

    added sort

--- src/Components/InstalledCard.jsx
+++ src/Components/InstalledCard.jsx
@@ -1,72 +1,55 @@
-import React from 'react';
+import React from "react";
 import downloadImg from "../assets/icon-downloads.png";
-import reviewImg from "../assets/icon-review.png";
 import ratingImg from "../assets/icon-ratings.png";
+import { getStoredApp, removeFromStorage } from "../Utility/LocalStorage";
 
-const InstalledCard = ({app}) => {
-    const {image, title, size, reviews, ratingAvg, downloads } = app
-    return (
-            <div className='bg-[#FFFFFF] py-2 flex justify-between items-center shadow-md rounded-xl px-3'>
+const InstalledCard = ({ app, setInstalledApps }) => {
+  const { image, title, size, id, ratingAvg, downloads } = app || {};
 
-                <div className='flex items-center'>
+  const handleUninstall = (id) => {
+    removeFromStorage(id);
+    setInstalledApps(getStoredApp());
+  };
 
-            <div>
+  return (
+    <div className="bg-[#FFFFFF] py-2 flex justify-between items-center shadow-md rounded-xl px-3">
+      <div className="flex items-center">
+        <div>
+          <img src={image} className="w-15 rounded-xl shadow-xs" alt="" />
+        </div>
 
-                <img src={image} className='w-15 rounded-xl shadow-xs' alt="" />
+        <div className="pl-2 ">
+          <h1 className="font-semibold">{title}</h1>
+
+          <div className="flex gap-3 items-center ">
+            <div className="flex items-center gap-1 ">
+              <img src={downloadImg} className="w-3.5" alt="" />
+
+              <h1 className=" text-green-600 font-semibold">{downloads}M</h1>
             </div>
+            <div className="flex items-center gap-1 ">
+              <img src={ratingImg} className="w-3.5" alt="" />
 
-
-  <div className='pl-2 '>
-                <h1 className='font-semibold'>{title}</h1>
-
-    <div className='flex gap-3 items-center '>
-
-         <div className="flex items-center gap-1 ">
-          <img src={downloadImg} className='w-3.5' alt="" />
-
-
-             <h1 className=" text-green-600 font-semibold">{downloads}M</h1>
-         </div>
-         <div className="flex items-center gap-1 ">
-          <img src={ratingImg} className='w-3.5' alt="" />
-
-
-             <h1 className=" text-[#FF8811] font-medium">{ratingAvg}</h1>
-         </div>
-         <div className="flex items-center gap-0.5  font-semibold text-gray-500">
-
-
-
-             <h1>{size}</h1><h1>GB</h1>
-         </div>
-    
-
-
-
-     </div>
-
-
-  </div>
-
-
-
-
-
-                </div>
-
-
-
-            <div>
-
-         <button className="bg-[#00D390] px-8 py-2 rounded-md font-semibold text-white text-lg shadow-md hover:shadow-lg cursor-pointer ">
-                Uninstall
-              </button>
-
+              <h1 className=" text-[#FF8811] font-medium">{ratingAvg}</h1>
             </div>
-
-
+            <div className="flex items-center gap-0.5  font-semibold text-gray-500">
+              <h1>{size}</h1>
+              <h1>GB</h1>
             </div>
-    );
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <button
+          onClick={() => handleUninstall(id)}
+          className="bg-[#00D390] px-8 py-2 rounded-md font-semibold text-white text-lg shadow-md hover:shadow-lg cursor-pointer "
+        >
+          Uninstall
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default InstalledCard;
--- src/pages/Installations.jsx
+++ src/pages/Installations.jsx
@@ -1,15 +1,43 @@
-import React, {useContext } from 'react';
+import React, {useContext,  useState } from 'react';
 import { getStoredApp } from '../Utility/LocalStorage';
 import { InstallContext } from '../Layout/MainLayout';
 
 import InstalledCard from '../Components/InstalledCard';
 const Installations = () => {
+    const [installedApps,setInstalledApps] =useState(getStoredApp())
 
-    const installedApps = getStoredApp();
     // // console.log(installedApps);
     // const [isInstalled,setIsInstalled] =useContext(InstallContext)
 
     // console.log(isInstalled);
+
+
+
+
+    const [sortOrder, setSortOrder] =useState('none')
+
+    const sortedApps =( ()=>{
+
+        if(sortOrder ==="download-desc"){
+        const sortedList = [...installedApps].sort((a,b) =>b.downloads -a.downloads )
+
+        return sortedList 
+        
+
+    }else if(sortOrder === "download-asc"){
+
+        const sortedList = [...installedApps].sort((a,b) =>a.downloads -b.downloads )
+
+       return sortedList
+    }else{
+        return installedApps
+    }
+
+    })()
+
+
+
+
 
     return (
         <div className='bg-[#E9E9E9] px-2 sm:px-4 lg:px-16'>
@@ -23,20 +51,48 @@ const Installations = () => {
 
     <div className=' flex justify-between items-center py-5'>
         <div>
-            <h1 className='text-xl font-semibold '>{installedApps.length} Apps found</h1>
+            <h1 className='text-xl font-semibold '>{sortedApps.length} Apps found</h1>
         </div>
         <div>
-            <button className='btn btn-primary'>hey het</button>
+
+
+            <label className='form-control w-full max-w-xs'>
+          <select
+            className='select select-bordered'
+            value={sortOrder}
+            onChange={e => setSortOrder(e.target.value)}
+          >
+            <option disabled value='none'>Sort by Download</option>
+
+            <option value='download-desc'>High-&gt;Low</option>
+
+            <option value='download-asc'>Low-&gt;High</option>
+
+          </select>
+        </label>
+
+
         </div>
     </div>
 
 
     
-    <div className='flex flex-col  gap-2'>
-
+    <div className='flex flex-col  gap-2 pb-6 min-h-[30vh]'>
          {
-             installedApps.map(app =><InstalledCard key={app.id} app={app} ></InstalledCard>)
+             sortedApps.map(app =><InstalledCard key={app.id} 
+             app={app} 
+             setInstalledApps={setInstalledApps}
+
+             ></InstalledCard>)
             }
+
+
+
+
+
+
+
+
 
     </div>
 
